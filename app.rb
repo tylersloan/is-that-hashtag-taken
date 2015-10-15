d8deaa1aa35d1a480558922c3159fb8cdfd8f7a6
Tyler Sloan <tyler.g.sloan@gmail.com>
2015-10-15 13:36:35 -0400

    change copy

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ get "/tags/:tag" do
   client = Instagram.client(:access_token => session[:access_token])
   
   tags = client.tag_search(params['tag'])
-  html = "<h2>Tag Name = #{tags[0].name}. Media Count =  #{tags[0].media_count}. </h2><br/><br/>"
+  html = "<h2>Hashtag: #{tags[0].name}</h2><h3>Used #{tags[0].media_count} times.</h3><br/><br/>"
   for media_item in client.tag_recent_media(tags[0].name)
     html << "<img src='#{media_item.images.thumbnail.url}'>"
   end
