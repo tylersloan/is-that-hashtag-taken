3bee9d23b6c70b0b270516b327649fd9324d7254
Tyler Sloan <tyler.g.sloan@gmail.com>
2015-10-15 13:09:07 -0400

    delete html

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ end
 
 get "/tags/:tag" do
   client = Instagram.client(:access_token => session[:access_token])
-  # html = "<h1>Search for tags, get tag info and get media by tag</h1><input id='search' type='text' />"
+  
   tags = client.tag_search(params['tag'])
   html = "<h2>Tag Name = #{tags[0].name}. Media Count =  #{tags[0].media_count}. </h2><br/><br/>"
   for media_item in client.tag_recent_media(tags[0].name)
