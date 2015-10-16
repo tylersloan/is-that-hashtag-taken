bcc3219b1523ddfb46922538f0732986b27c6eb1
Tyler Sloan <tyler.g.sloan@gmail.com>
2015-10-15 23:43:28 -0400

    use dotenv for hiding keys. return last 20 tweets with teh hastag

--- app.rb
+++ app.rb
@@ -1,28 +1,47 @@
 require "sinatra"
 require "instagram"
 require "twitter"
+require 'dotenv'
+Dotenv.load
 
-load 'instagram_config.rb'
-load 'twitter_config.rb'
+Instagram.configure do |config|
+  config.client_id = ENV['INSTAGRAM_CLIENT_ID']
+end
+
+twitterClient = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+end
 
 enable :sessions
-
-CALLBACK_URL = "http://localhost:4567/oauth/callback"
 
 get "/" do
   erb :index
 end 
 
 get "/:tag" do
-  client = Instagram.client(:access_token => session[:access_token])
+  theTag = params['tag']
+  
+  instagramClient = Instagram.client(:access_token => session[:access_token])
+  
   html = "<form action=''>
   <input type='text' placeholder='search hashtag'>
   <button type='submit' id='js-search-hashtag'>Search</button>
   </form>"
-  tags = client.tag_search(params['tag'])
+
+  tags = instagramClient.tag_search(theTag)
+  
   html = "<h2>Hashtag: #{tags[0].name}</h2><h3>Used #{tags[0].media_count} times.</h3><br/><br/>"
-  for media_item in client.tag_recent_media(tags[0].name)
+  
+  for media_item in instagramClient.tag_recent_media(tags[0].name)
     html << "<img src='#{media_item.images.thumbnail.url}'>"
   end
+
+  twitterSearch = twitterClient.search("%23" + theTag, result_type: "mixed").take(20)
+
+  for tweet in twitterSearch
+    html << "<li>#{ tweet.text }</li>"
+  end
+
   html
 end
