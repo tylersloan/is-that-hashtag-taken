169ba2c193d25468711ccc1a5294acb613b7af33
Tyler Sloan <tyler.g.sloan@gmail.com>
2015-12-15 22:22:51 -0500

    made instagram thumbnails links to instagram post

--- app.rb
+++ app.rb
@@ -17,30 +17,25 @@ enable :sessions
 
 get "/" do
   erb :index
-end 
+end
 
 get "/:tag" do
+  erb :index
   theTag = params['tag']
-  
+
   instagramClient = Instagram.client(:access_token => session[:access_token])
-  
-  html = "<form action=''>
-  <input type='text' placeholder='search hashtag'>
-  <button type='submit' id='js-search-hashtag'>Search</button>
-  </form>"
 
   tags = instagramClient.tag_search(theTag)
-  
+
   html = "<h2>Hashtag: #{tags[0].name}</h2><h3>Used #{tags[0].media_count} times.</h3><br/><br/>"
-  
   for media_item in instagramClient.tag_recent_media(tags[0].name)
-    html << "<img src='#{media_item.images.thumbnail.url}'>"
+    html << "<a href='#{media_item.link}'><img src='#{media_item.images.thumbnail.url}'></a>"
   end
 
-  twitterSearch = twitterClient.search("#" + theTag + " -rt", result_type: "mixed").take(20)
 
+  twitterSearch = twitterClient.search("#" + theTag + " -rt", result_type: "mixed").take(20)
   for tweet in twitterSearch
-    html << "<li>#{ tweet.text }</li>"
+    html << "<li>#{ tweet.hashtags }</li>"
   end
 
   html
